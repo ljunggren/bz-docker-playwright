c3f4b06ca64f397c92d9566f74db44b6f2bca804
ljunggren <mljunggren@gmail.com>
2020-11-01 19:28:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,177 @@
+#! /usr/bin/env node
+// Device Descriptors https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
+
+
+(async () => {
+
+const { chromium } = require('playwright');
+const options = require('node-options');
+const { saveVideo } = require('playwright-video');
+const Service = require('./logService').Service;
+
+// Command defaults
+const opts = {
+  "verbose" : false,
+  "file": "report",
+  "listscenarios":"",
+  "listsuite":"",
+  "device" : "",
+  "screenshot": false,
+  "token":"",
+  "userdatadir":"test",
+  "width":1280,
+  "height":1024,
+  "docker": false,
+  "gtimeout": "",
+  "notimeout": false,
+  "timeout": "",
+  "video": false
+}
+
+// Remove the first two arguments, which are the 'node' binary and the name
+// of your script.
+const result = options.parse(process.argv.slice(2), opts);
+const verbose = opts.verbose;
+const token = opts.token;
+const docker = opts.docker;
+let userdatadir = opts.userdatadir;
+const width = opts.width;
+const height = opts.height;
+const gtimeout=opts.gtimeout;
+const listscenarios=opts.listscenarios;
+const listsuite=opts.listsuite;
+const notimeout=opts.notimeout;
+const timeout= opts.timeout;
+const video = opts.video;
+let file = opts.file;
+
+if (result.errors || !result.args || result.args.length !== 1) {
+  console.log('USAGE: boozang [--token] [--docker] [--gtimeout] [--notimeout] [--verbose] [--userdatadir] [--listscenarios] [--listsuite] [--width] [--height] [--screenshot] [--file=report] [url]');
+  process.exit(2);
+}
+
+console.log("Running with following args");
+console.log(opts);
+console.log("Example: Use --verbose for verbose logging (boolean example). Use --width=800 to override default width (value example.)");
+
+
+  file = (docker ? "/var/boozang/" : "") + opts.file;
+
+  if (opts.userdatadir){
+    userdatadir = (docker ? "/var/boozang/userdatadir" : "") + (opts.userdatadir || "");
+    console.log("Setting userdatadir: " + userdatadir);
+  }
+  
+  const launchargs = [
+    '--disable-extensions-except=' + __dirname + '/bz-extension',
+    '--load-extension=' + __dirname + '/bz-extension',
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    `--window-size=${width},${height}`,
+    '--defaultViewport: null',
+    ];
+
+  const browser = await chromium.launchPersistentContext("bananas",{
+    headless: false,
+    args: launchargs,
+    launchType: "PERSISTENT"
+    /** ,
+    userDataDir: userdatadir,
+     */
+  });
+
+  function printStackTrace(app,err){
+    console.error(
+      "\n#######################################\n"
+    + app + " error: " + err.message
+    + "\n#######################################\n"
+    );   
+  }
+
+  function appPrintStackTrace(err){
+    printStackTrace("app",err);
+  }
+
+  function idePrintStackTrace(err){
+    printStackTrace("ide",err);
+  }
+
+
+
+  // Setup popup
+  //let popup = null;
+  function setupPopup(popup) {
+    // popup = pages[pages.length-1]; 
+    popup.setViewportSize({
+      width: parseInt(width),
+      height: parseInt(height)
+    });
+
+    popup.on("error", appPrintStackTrace);
+    popup.on("pageerror", appPrintStackTrace);
+    Service.setPopup(popup)
+  }
+
+
+  // let pages = await browser.pages();
+  // browser.on('targetcreated', async () => {
+    // console.log("New pop window *******")
+        // //console.log('New window/tab event created');
+        // pages = await browser.pages();
+        // //console.log("Pages length " + pages.length);
+        // setupPopup(); 
+        // Service.setPage(page);  
+  // });
+
+
+
+  const page = await browser.newPage();
+
+  page.on('popup', async dialog => {
+    setupPopup(dialog)
+  })
+
+  //await capture.stop()
+  //await browser.close();
+
+
+
+  // Assign all log listeners
+  Service.logMonitor(page,notimeout,gtimeout,timeout,file, browser,video)
+  if(listsuite||listscenarios){
+    Service.setBeginningFun(function(){
+      Service.insertFileTask(function(){
+        Service.result = 0;
+        Service.shutdown()
+      })
+      if(listsuite){
+        page.evaluate((v)=>{
+          $util.getTestsBySuite(v)
+        }, listsuite);
+      }else if(listscenarios){
+        page.evaluate((v)=>{
+          $util.getScenariosByTag(v)
+        }, JSON.parse(listscenarios));
+      }
+    })
+  }
+
+
+  let url = result.args[0];
+  if ((!opts.screenshot) && (!opts.listscenarios) && typeof (url) == 'string' && !url.endsWith("/run")) {
+    if (!url.endsWith("/")) {
+        url += "/"
+    }
+    url += "run"
+}
+  const response = await page.goto(url);
+
+  page.on("error", idePrintStackTrace);
+  page.on("pageerror", idePrintStackTrace);
+
+})()
+
+
+
+
+
---
+++ logService.js
@@ -0,0 +1,275 @@
+const fs = require('fs');
+const { saveVideo } = require('playwright-video');
+
+const Service = {
+  stdTimeout:60000,
+  taskMap:{},
+  timer:0,
+  status:"",
+  capture:"",
+  browser:"",
+  result: 2,
+  logMonitor(page,notimeout,gtimeout,stdTimeout,reportPrefix, browser,video){
+    Service.notimeout=notimeout
+    Service.page=page
+    Service.video=video
+    
+    console.log("Initializing logMonitor");
+    gtimeout && console.log("Override global timeout: " + gtimeout + " mins");
+    stdTimeout && console.log("Override action timeout: " + stdTimeout + " mins");
+    reportPrefix && console.log("Override report prefix: " + reportPrefix);
+
+    Service.browser = browser;
+ 
+    Service.stdTimeout=stdTimeout*60000||60000;
+    Service.reportPrefix=reportPrefix ? reportPrefix + "_":"";
+    
+    if(!notimeout&&gtimeout){
+      setTimeout(()=>{
+        Service.gracefulShutdown("Global timeout triggered - try to do graceful shutdown")
+      },gtimeout*60000)
+    }
+    if(notimeout){
+      clearTimeout(Service.status)
+    }
+
+    page.on('console', msg => {
+      let timeout,t;
+
+      msg = (!!msg && msg.text()) || "def";
+      msg=trimPreMsg(msg)
+      if(!msg){
+        return
+      }
+      
+      if(Service.curTask){
+        t=Service.curTask
+        Service.curTask=0
+      }else{
+        for(let key in Service.taskMap){
+          if(msg.includes(key)){
+            t=Service.taskMap[key]
+            
+            break
+          }
+        }
+      }
+      
+      if(t){
+        if (!t.noLog){
+          console.log(msg)
+        }
+        clearTimeout(Service.timer)
+        if(!t.timeout){
+          timeout=t.fun(msg)
+          //console.log("Get timeout: "+timeout)
+        }else{
+          timeout=t.timeout
+        }
+        
+        if(!notimeout){
+          // console.log("set timeout for shutdown: "+timeout)
+          Service.timer=setTimeout(()=>{
+            Service.shutdown(t.msg)
+          },timeout)
+        }
+        
+        t.timeout&&t.fun(msg,Service.timer)
+        if(t.oneTime){
+          Service.removeTask(t)
+        }
+      }
+    })
+    
+    function trimPreMsg(msg){
+      if(msg&&msg.startsWith("BZ-LOG:")){
+        msg=msg.substring(7).trim()
+      }else{
+        return
+      }
+      return msg
+    }
+  },
+  setBeginningFun(fun){
+    Service.beginningFun=fun
+  },
+  setPopup(popup){
+    console.log("set popup window")
+    this.popup=popup
+  },
+  //task:{key,fun,onTime,timeout}
+  addTask(task){
+    this.taskMap[task.key]=task
+  },
+  removeTask(task){
+    delete this.taskMap[task.key]
+  },
+  init(){
+    this.status=setTimeout(()=>{
+      if(Service.status!="ready"){
+        Service.shutdown("Failed to load test")
+      }
+    },Service.stdTimeout)
+    
+    Service.addTask({
+      key:"ready",
+      fun(){
+        Service.status="ready"
+        console.log("Ready on logService")
+        if(Service.beginningFun){
+          Service.beginningFun()
+        }else{
+          Service.setRunTasks()
+        }
+        if(Service.video){
+          Service.page.evaluate((v)=>{
+            BZ.requestVideo()
+          });
+        }
+      },
+      oneTime:1,
+      timeout:Service.stdTimeout
+    })
+  },
+  setRunTasks(){
+    Service.taskMap={}
+    Service.addTask({
+      key:"ms:",
+      fun(msg){
+        return (parseInt(msg.split(this.key)[1].trim())||0)+15000
+      },
+      msg:"Action timeout"
+    })
+
+    Service.addTask({
+      key:"app-run:",
+      fun(msg){
+        Service.popup.evaluate(()=>{ msg;  });
+      },
+      timeout:Service.stdTimeout
+    })
+
+    Service.addTask({
+      key:"ide-run:",
+      fun(msg){
+        Service.page.evaluate(()=>{ msg;  });
+      },
+      timeout:Service.stdTimeout
+    })
+
+    Service.addTask({
+      key:"videostart:",
+      fun(msg){
+        (async () => {
+          let videoFile = msg.split("videostart:")[1]+".mp4";
+          console.log("Start recording video: ", videoFile);
+           Service.capture = await saveVideo(Service.popup, Service.reportPrefix + videoFile, {followPopups:true, fps: 5});      
+        })()
+      },
+      timeout:Service.stdTimeout
+    })
+
+    Service.addTask({
+      key:"videostop:",
+      fun(msg){
+        (async () => {
+          let videoFile = msg.split("videostop:")[1]+".mp4";
+          console.log("Stop recording video: ", videoFile);
+          await Service.capture.stop();
+          await (()=>{
+            Service.page.evaluate((v)=>{
+              BZ.savedVideo()
+            });
+
+          })()
+        })()
+      },
+      timeout:Service.stdTimeout
+    })
+    
+
+
+    Service.addTask({
+      key:"screenshot:",
+      fun(msg){
+        let screenshotFile = msg.split("screenshot:")[1]+".png";
+        Service.popup.screenshot({path: screenshotFile});
+      },
+      timeout:Service.stdTimeout
+    })
+
+    Service.addTask({
+      key:"Stop task!",
+      fun(msg){
+        Service.setEndTasks()
+      },
+      timeout:Service.stdTimeout
+    })
+  },
+  setEndTasks(){
+    Service.taskMap={}
+    Service.addTask({
+      key:"Result:",
+      fun(msg){
+        msg=msg.split("Result:")[1].trim()
+        Service.result = msg == "Success" ? 0:2;
+        console.log("Exit with status code: ", Service.result);
+      },
+      timeout:Service.stdTimeout
+    })
+    Service.addTask({
+      key:"All tests completed!",
+      fun(msg){
+        Service.shutdown(this.key)
+      }
+    })
+    this.insertFileTask()
+  },
+  insertFileTask(exFun){
+    Service.addTask({
+      key:"BZ-File output:",
+      fun(msg){
+        msg=msg.substring(this.key.length).trim()
+        if(!this.name){
+          this.name=Service.reportPrefix + msg.toLowerCase().replace(/\ /g,"_");
+        }else if(msg=="end"){
+          let name=this.name
+          fs.writeFile(name, this.content, (err)=>{
+            if (err) {
+              Service.shutdown("Error: on output file: "+name+", "+ err.message)
+            }
+            console.log("Report "+name+" saved.")
+            if(exFun){
+              exFun()
+            }
+          })
+          this.name=0
+        }else{
+          this.content=msg
+        }
+      },
+      timeout:Service.stdTimeout,
+      noLog:1
+    })
+  },
+  shutdown(msg){
+    msg && console.log(msg);
+    Service.browser.close();
+    if(!this.notimeout){
+      process.exit(Service.result)
+    }
+  },
+  gracefulShutdown(msg){
+    console.error("Try to get Boozang to exit gracefully and write report");
+    Service.popup.screenshot({path: "graceful_shutdown.png"});
+    Service.page.evaluate(()=>{  
+      BZ.e();console.log("BZ-LOG: Timing out check IDE response"); 
+    });
+    // Wait 100 seconds for Boozang to finish before force kill
+    setTimeout(function(){
+      Service.shutdown(msg);
+    },100000)   
+  }
+}
+Service.init()
+exports.Service = Service;
